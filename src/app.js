cf4a5db9df7bb75f7830ed81c4c4cacf2d387b0a
Moniya03 <moniyadhingra1703@gmail.com>
2025-04-13 07:26:17 +0000

    add controllers

--- src/app.js
+++ src/app.js
@@ -12,5 +12,13 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes import
+import userRouter from"./routes/user.routes.js"
+
+// routes declarataion
+app.use("/api/v1/users",userRouter)
+
+//http://localhost:8000/users/api/v1/users/regsiters
  
 export {app}
